e434149fe850e2475b01ba24b1dff808d9ad13f9
GigaOrts <ortsortskhoev95@gmail.com>
2023-09-08 21:59:29 +0300

    Update ChessGridNavigator.cs

--- Assets/App/Scripts/Scenes/SceneChess/Features/GridNavigation/Navigator/ChessGridNavigator.cs
+++ Assets/App/Scripts/Scenes/SceneChess/Features/GridNavigation/Navigator/ChessGridNavigator.cs
@@ -10,8 +10,20 @@ namespace App.Scripts.Scenes.SceneChess.Features.GridNavigation.Navigator
     {
         public List<Vector2Int> FindPath(ChessUnitType unit, Vector2Int from, Vector2Int to, ChessGrid grid)
         {
-            //напиши реализацию не меняя сигнатуру функции
-            throw new NotImplementedException();
+            foreach (var item in grid.Pieces)
+            {
+                Debug.Log($"PieceModel: {item.PieceModel} CellPosition: {item.CellPosition} View:{item.View}");
+            }
+
+            Debug.Log($"SIZE: {grid.Size}");
+            Debug.Log($"unit: {unit}");
+            Debug.Log($"from: {from}");
+            Debug.Log($"to: {to}");
+
+            if (unit == ChessUnitType.Pon)
+                return new List<Vector2Int>() { new Vector2Int(7, 7) };
+            else
+                return null;
         }
     }
 }
